deab39add211348585913ff5131268b1ab77bed4
David Vezzani <david_vezzani@yahoo.com>
2018-01-15 13:24:36 -0700

    CRUD for user

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
   <div id="app">
     <img src="./assets/logo.png">
     <button @click="clickButton">asdf</button>
+    <button @click="newUser">New User</button>
+    <button @click="updateUser">Update User</button>
+    <button @click="deleteUser">Delete User</button>
     <router-view/>
   </div>
 </template>
@@ -17,11 +20,29 @@ export default {
     },
     got_it: function(val){
       console.log("got it: " + JSON.stringify(val));
+    },
+    user_created: function(val){
+      console.log("user created: " + JSON.stringify(val));
+    },
+    user_updated: function(val){
+      console.log("user updated: " + JSON.stringify(val));
+    },
+    user_destroyed: function(val){
+      console.log("user destroyed: " + JSON.stringify(val));
     }
   },
   methods: {
     clickButton: () => {
       (new Vue()).$socket.emit('emit_method', {asdf: 2});
+    }, 
+    newUser: () => {
+      (new Vue()).$socket.emit('create_user', {name: "dcvezzani", notes: "David Vezzani", freeDay: 0, freeMeal: false, sickDay: 0});
+    }, 
+    updateUser: () => {
+      (new Vue()).$socket.emit('update_user', {id: 3, name: "dcvezzani", notes: "David Vezzani", freeDay: 0, freeMeal: false, sickDay: 0});
+    }, 
+    deleteUser: () => {
+      (new Vue()).$socket.emit('destroy_user', {id: 3});
     }
   }
 }
