598f961f7de8721092639315384826bbda4be60f
David Vezzani <david_vezzani@yahoo.com>
2018-01-15 08:58:37 -0700

    example event listener and emitter

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <button @click="clickButton">asdf</button>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  sockets:{
+    connect: function(){
+      console.log('socket connected')
+    },
+    got_it: function(val){
+      console.log("got it: " + JSON.stringify(val));
+    }
+  },
+  methods: {
+    clickButton: () => {
+      (new Vue()).$socket.emit('emit_method', {asdf: 2});
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import socketio from 'socket.io-client';
+import VueSocketIO from 'vue-socket.io';
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
 
+export const SocketInstance = socketio('http://127.0.0.1:3000');
+Vue.use(VueSocketIO, SocketInstance)
+
 /* eslint-disable no-new */
-new Vue({
+var app = new Vue({
   el: '#app',
   router,
   components: { App },
